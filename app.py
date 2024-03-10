c2da1210310b9197c1f077d25529d0076fd27734
Spec.psx <69973760+Spectrewolf8@users.noreply.github.com>
2024-03-10 22:57:07 +0500

    Refactor email validation and display results

--- app.py
+++ app.py
@@ -14,17 +14,25 @@ def home():
 @app.route("/validate-email", methods=["GET", "POST"])
 def validate_emails():
     if request.method == "POST":
-        context={
-
-        }
+        context = {"verification_results": []}
         if request.form["payload_type"] == "emails":
             # print(request.form["emails"].split(","))
             emails = request.form["emails"].split(",")
+            check_deliverablility = request.form["checkDeliverability"]
             for email in emails:
-                 evaluate_email
-            
+                evaluation_result = evaluate_email(email, check_deliverablility)
+                context["verification_results"].append(evaluation_result)
+
         elif request.form["payload_type"] == "email":
-            print(request.form["email"])
+            email = request.form["email"]
+            check_deliverablility = request.form["checkDeliverability"]
+            evaluation_result = evaluate_email(email, check_deliverablility)
+            context["verification_results"].append(evaluation_result)
+
+        print(context)
+        return render_template(
+            "results.html", verification_results=context["verification_results"]
+        )
 
     return render_template("index.html")
 
@@ -36,25 +44,31 @@ def display_results():
     return render_template("results.html")
 
 
-def evaluate_email(checkDeliverability=True):
+def evaluate_email(email, checkDeliverability=True):
+    status = {"email": "", "status": "", "message": ""}
     try:
 
-            # Check that the email address is valid. Turn on check_deliverability
-            # for first-time validations like on account creation pages (but not
-            # login pages).
-            emailinfo = validate_email(email, check_deliverability=checkDeliverability)
-
-            # After this point, use only the normalized form of the email address,
-            # especially before going to a database query.
-            email = emailinfo.normalized
-            print("Valid:", email, "\ninfo:", emailinfo)
+        # Check that the email address is valid. Turn on check_deliverability
+        # for first-time validations like on account creation pages (but not
+        # login pages).
+        status["email"] = email
+        emailinfo = validate_email(email, check_deliverability=checkDeliverability)
+        if checkDeliverability:
+            status["status"] = "Valid/deliverable"
+            status["message"] = "Email is valid and deliverable"
+        else:
+            status["status"] = "Valid"
+            status["message"] = "Email is valid"
 
     except EmailNotValidError as e:
 
-            # The exception message is human-readable explanation of why it's
-            # not a valid (or deliverable) email address.
-            print(str(e))
- 
+        # The exception message is human-readable explanation of why it's
+        # not a valid (or deliverable) email address.
+        print(str(e))
+        status["status"] = "Invalid"
+        status["message"] = str(e)
+    return status
+
 
 @app.route("/favicon.ico")
 def favicon():
